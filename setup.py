32e37cc7021c3e7a6b9d2ab509fc2a6437408e8e
Jordi Mas <jmas@softcatala.org>
2021-09-06 16:14:06 +0200

    Initial version

---
+++ setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="tmx-to-text",
+    version="0.1.0",
+    description="Converts TMX files to text files",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jordimas/tmx-to-text",
+    author="Jordi Mas",
+    author_email="jmas@softcatala.org",
+    license="GPLv2+",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["src/tmx-to-text"],
+    include_package_data=True,
+    install_requires=["lxml"],
+    entry_points={
+        "console_scripts": [
+            "tmx-to-text=__main__:main",
+        ]
+    },
+)
+
