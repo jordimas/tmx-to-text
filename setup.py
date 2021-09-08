871e972cfee829abaf6d9d58cbc6ea99a3be0b05
Jordi Mas <jmas@softcatala.org>
2021-09-08 05:18:52 +0200

    0.1.3 release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="tmx-to-text",
-    version="0.1.2",
+    version="0.1.3",
     description="Converts TMX files to text files",
     long_description=README,
     long_description_content_type="text/markdown",
