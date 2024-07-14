13f0e49644811e436c17807098b6bda38da70838
Jordi Mas <jmas@softcatala.org>
2024-07-14 18:54:29 +0200

    Add Python 3.12 to the spec

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         'Programming Language :: Python :: 3.11',
+        "Programming Language :: Python :: 3.12",
     ],
     packages=["src/tmx_to_text"],
     include_package_data=True,
